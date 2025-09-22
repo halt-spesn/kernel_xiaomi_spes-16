3a91d6afdce47fd09b39492adf1d48855994e0b7
halt-spesn <ffoxy2870@gmail.com>
2025-09-22 21:59:33 +0300

    updates

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1236,17 +1236,20 @@ int subsystem_restart_dev(struct subsys_device *dev)
 		return -EBUSY;
 	}
 
-	pr_info("Restart sequence requested for %s, restart_level = %s.\n",
-		name, restart_levels[dev->restart_level]);
+	if (!strcmp(name, "modem")) {
+		dev->restart_level = RESET_SUBSYS_COUPLED;
+		pr_warn("Restart sequence requested for %s, force restart_level to = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	} else {
+		pr_info("Restart sequence requested for %s, restart_level = %s.\n",
+			name, restart_levels[dev->restart_level]);
+	}
 
 	if (disable_restart_work == DISABLE_SSR) {
 		pr_warn("subsys-restart: Ignoring restart request for %s\n",
 									name);
 		return 0;
 	}
-
-	if (!strcmp(name, "modem"))
-		dev->restart_level = RESET_SUBSYS_COUPLED;
 
 	switch (dev->restart_level) {
 
